58b289abd614e99503613c3afff5a44687d69115
Mederbek Dzhumabaev <65927619+MederD@users.noreply.github.com>
2024-06-10 13:10:28 -0700

    Update index.py

--- GetWafActivityStack/lambda/code/index.py
+++ GetWafActivityStack/lambda/code/index.py
@@ -1,9 +1,9 @@
 import boto3
 from log_wrapper import LogWrapper
+from secret_wrapper import SecretsWrapper
 import requests
 import os
 
-secretsmanager = boto3.client('secretsmanager')
 client_sns = boto3.client('sns')
 sns_topic = os.environ['sns_topic']
 secretname = os.environ['secretname']
@@ -11,10 +11,8 @@ logName = os.environ['logGroupName']
 query_string = os.environ['query']
 
 log_wrapper = LogWrapper(log_client = boto3.client('logs', region_name = 'us-east-1'))
-
-# Retrieve the secret value
-response = secretsmanager.get_secret_value(SecretId = secretname)
-vt_api = response['SecretString']
+secret_wrapper = SecretsWrapper(secret_client = boto3.client('secretsmanager', region_name = 'us-east-1'))
+vt_api = secret_wrapper.get_secrets(secretname)
 
 def handler(event, context):
     try:
@@ -30,7 +28,7 @@ def handler(event, context):
         response = requests.get(
             url = f"https://www.virustotal.com/api/v3/ip_addresses/{ip}",
             headers={
-                'x-apikey': vt_api
+                'x-apikey': vt_api['virustotalkey']
             }
         )
 
@@ -39,20 +37,18 @@ def handler(event, context):
             malicious = data['attributes']['last_analysis_stats']['malicious']
             suspicious = data['attributes']['last_analysis_stats']['suspicious']
 
-            if malicious > 0 or suspicious > 0:
-                message = {
-                    "ip": data['id'],
-                    "malicious": malicious,
-                    "suspicious": suspicious
-                }
+            log_wrapper.logger.info("%s report -> malicious: %s, suspicious: %s", data['id'], malicious, suspicious)
 
-            log_wrapper.logger.info(" --- Constructing message %s. --- ", message)
+            if malicious > 0 or suspicious > 0:
+                message = "IP: %s, Malicious: %s, Suspicious: %s" % (data['id'], malicious, suspicious)
+                
+                log_wrapper.logger.info(" --- Constructing message for %s --- ", data['id'])
         
-            client_sns.publish(
-                TargetArn = topic,
-                Message = message,
-                Subject = 'Log query results'
-            )
+                client_sns.publish(
+                    TargetArn = topic,
+                    Message = message,
+                    Subject = 'Log query results'
+                )
         
         else:
             log_wrapper.logger.info(" --- No recorded malicious or suspicious report on virustotal of IP: %s. --- ", ip)  
