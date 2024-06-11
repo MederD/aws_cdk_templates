a8987d768405df7b9e68f66600928c93ba4a72e8
Mederbek Dzhumabaev <65927619+MederD@users.noreply.github.com>
2024-06-11 10:02:55 -0700

    Update secret_wrapper.py

--- GetWafActivityStack/lambda/code/secret_wrapper.py
+++ GetWafActivityStack/lambda/code/secret_wrapper.py
@@ -4,6 +4,7 @@ class SecretsWrapper:
     def __init__(self, secret_client, instance = None):
         self.secret_client = secret_client
         self.instance = instance
+        self._cache = {}
 
     @classmethod
     def from_resource(cls):
@@ -11,9 +12,10 @@ class SecretsWrapper:
         return cls(secret_client)
 
     def get_secrets(self, secretId):
-        try:
-            _secret = json.loads(self.secret_client.get_secret_value(SecretId = secretId)['SecretString'])
-            return _secret
-        
-        except Exception as e:
-            raise e
+        if secretId not in self._cache:
+            try:
+                _secret = json.loads(self.secret_client.get_secret_value(SecretId = secretId)['SecretString'])
+                self._cache[secretId] = _secret
+            except Exception as e:
+                raise e
+        return self._cache[secretId]
